52f4b81bfefeb065054421235d640acb617095d8
Choo Kein Yie <kein.yie@gmail.com>
2024-10-18 15:36:49 +0800

    chore: setup files for multiple screens

--- app/src/main/java/recipe_saver/inti/myapplication/HomeActivity.java
+++ app/src/main/java/recipe_saver/inti/myapplication/HomeActivity.java
@@ -3,17 +3,12 @@ package recipe_saver.inti.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.activity.EdgeToEdge;
 
-
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        EdgeToEdge.enable(this);
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
-        finish();
+        setContentView(R.layout.activity_home);
     }
 }
---
+++ app/src/main/java/recipe_saver/inti/myapplication/LoginActivity.java
@@ -0,0 +1,27 @@
+package recipe_saver.inti.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity{
+    private Button mLoginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mLoginButton = findViewById(R.id.login_button);
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
