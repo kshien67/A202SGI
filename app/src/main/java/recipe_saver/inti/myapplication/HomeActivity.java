34ec7f7ffdd5e3940f8435525d7278ca2170c1f0
Adeline Too <adelinetoo02@gmail.com>
2024-10-24 17:13:14 +0800

    feat: added add recipe page

--- app/src/main/java/recipe_saver/inti/myapplication/HomeActivity.java
+++ app/src/main/java/recipe_saver/inti/myapplication/HomeActivity.java
@@ -1,9 +1,11 @@
 package recipe_saver.inti.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -23,9 +25,15 @@ public class HomeActivity extends AppCompatActivity {
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new DashboardFragment()).commit();
             } else if (id == R.id.navigation_ranking) {
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new RankingFragment()).commit();
-            } /*else if (id == R.id.navigation_profile) {
+            } else if (id == R.id.navigation_add) {
+                // Navigate to AddRecipeActivity
+                Intent intent = new Intent(HomeActivity.this, AddRecipeActivity.class);
+                startActivity(intent);
+            } else if (id == R.id.navigation_collection) {
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFragment()).commit();
-            }*/
+            } else if (id == R.id.navigation_settings) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SettingsFragment()).commit();
+            }
             return true;
         });
     }
---
+++ app/src/main/java/recipe_saver/inti/myapplication/SettingsFragment.java
@@ -0,0 +1,6 @@
+package recipe_saver.inti.myapplication;
+
+import androidx.fragment.app.Fragment;
+
+public class SettingsFragment extends Fragment {
+}
